8a24c556b3267cbd00795023fa178dbf39ac02a4
catalina-alzamora <alzamoracatalina@gmail.com>
2019-11-20 14:05:02 -0300

    primer commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,70 @@
+
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-    
+  encode: (offset, text) => {
+
+    //cuando no se ingresan las variables
+    if(offset==="" || text===""){
+      return(alert("Recuerda escribir la CLAVE y tu MENSAJE. Presiona el botón REINICIAR para continuar."));
+    }
+    //offset 0 es inválido
+    if(offset===0){
+      return(alert("Tu clave debe ser distinta de cero"))
+    }
+    //declarando variable para mostrar texto cifrado
+    let newText= "";
+
+    //recorrer texto para aplicar fórmula
+    for (let i = 0; i <= text.length; i++) {
+
+      //mayúsculas
+      if (text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90) {
+        newText += String.fromCharCode((text.charCodeAt(i) - 65 + offset) % 26 + 65);
+
+      //minusculas
+      } else if (text.charCodeAt(i) >= 97 && text.charCodeAt(i) <= 122) {
+        newText += String.fromCharCode((text.charCodeAt(i) - 97 + offset) % 26 + 97);
+      
+      //todo lo demás
+      } else { 
+        newText += text.charAt(i);
+         
+      } 
+    }  
+  //imprimir texto codificado al final
+  return newText;
   },
-  decode: () => {
-    /* Acá va tu código */
+
+  decode: (offset, text) => {
+    let newText= "";
+    for (let i = 0; i <= text.length; i++) {
+
+      if(offset==="" || text===""){
+        return(alert("Recuerda primero escribir la clave y tu mensaje. Presiona el botón Reiniciar para continuar."));
+      }
+      if(offset===0){
+        return(alert("Tu clave debe ser distinta de cero"))
+      }
+      //mayúsculas
+      if (text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90) {
+        newText += String.fromCharCode((text.charCodeAt(i) - 90 - offset) % 26 + 90);
+      //minusculas
+      } else if (text.charCodeAt(i) >= 97 && text.charCodeAt(i) <= 122) {
+        newText += String.fromCharCode((text.charCodeAt(i) - 122 - offset) % 26 + 122);
+      //todo lo demás
+      } else {
+        newText += text.charAt(i);
+      } 
+    }
+    //imprimir texto decodificado al final
+    return newText;
   }
-};
+} 
+
+/*
+fórmula: para cipher sería= [pasar a letra((letras en ascii) - 65 + offset) % 26 + 65)]
+Para descifrar se usa la misma fórmula pero en cambio se resta el offset 
+y se usa la posición final del grupo de caracteres usados
+-variable.charCodeAt(i)=sirve para obtener el ascii de cada letra
+-String.fromcharCode(i)=sirve para obtener la letra desde un número ascii
+-charAt(i)=sirve para que aparezca la letra de una determinada posición
+*/
--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-/*Acá va tu código*/
+
+  //Parte de encode
+
+    //obteniendo botón de encode y añadiendo función con evento click
+    document.getElementById('btnEncode').addEventListener('click', () => {
+
+   //asignando variable para guardar offset o clave (tipo number y número entero absoluto)
+    let keyNumber= Math.floor(Math.abs(parseInt(document.getElementById('key').value)));
+
+   //asignando variable para guardar texto inicial
+    let textArea1= document.getElementById('initialText').value;
+    
+   //asignando variable para llamar texto final 
+    let textArea2= document.getElementById('finalText');
+
+   //mostrando resultado en textarea de texto final 
+   textArea2.innerHTML= cipher.encode(keyNumber,textArea1);
+    });
+
+   //Parte de decode
+  document.getElementById('btnDecode').addEventListener('click', () => {
+  let keyNumber= parseInt(document.getElementById('key').value);
+  
+  let textArea1= document.getElementById('initialText').value;
+ 
+  let textArea2= document.getElementById('finalText');
+  textArea2.innerHTML= cipher.decode(keyNumber,textArea1);
+  });
+
